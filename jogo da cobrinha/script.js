6d9e8ebfcd04fde6ce9626cecdbb69b34600993f
Andre da Silva Carmona <104648375+DevCarmona@users.noreply.github.com>
2022-12-15 10:27:52 -0300

    Update script.js

--- jogo da cobrinha/script.js
+++ jogo da cobrinha/script.js
@@ -1,6 +1,11 @@
 let canvas = document.getElementById("snake")
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
 function criarBG(){
     // Estilo do contexto
@@ -9,4 +14,12 @@ function criarBG(){
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+function criaCobrinha(){
+    for(i = 0; i < snake.length; i++){
+        context.fillStyle = "green";
+        context.fillRect (snake[i].x, snake[i].y, box, box);
+    }
+}
+
 criarBG();
+criaCobrinha();
